b149846ceefcf1dd9d5ae48d9a06e9458a7a46e4
Kenan Christian <christian.dimas.94@gmail.com>
2018-10-03 21:54:22 +0700

    Add Hello World in JS

---
+++ Javascript/hello_world.js
@@ -0,0 +1,6 @@
+const helloWorlds = ["こんにちは世界", "salve Orbis Terrarum", "Hello World", "Hello Wêreld", "Përshendetje Botë", "مرحبا بالعالم", "你好，世界", "Ahoj světe",
+                 "Bonjour le monde", "Hallo Welt", "Ciao mondo", "Witaj świecie", "Привет мир", "Hallo Wereld"]
+
+helloWorlds.forEach((phrase) => {
+    console.log(phrase)
+})
