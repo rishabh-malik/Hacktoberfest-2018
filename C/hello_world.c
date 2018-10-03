d927d0378a08877cda05db906e34f5ea6cc3fef0
Åke Forslund <ake.forslund@gmail.com>
2018-10-03 17:36:01 +0200

    Add hello world example in C

---
+++ C/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Simple hello world program
+// Build with
+// gcc -o hello_world hello_world.c
+// and run with
+// ./hello_world
+int main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
