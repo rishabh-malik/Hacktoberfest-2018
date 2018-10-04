fcb23e800de6857fecc60f8a667476ee2f2f4044
anshul-negi <anshul0217@gmail.com>
2018-10-05 03:20:21 +0530

    Infinite random loop in c#

---
+++ C/infinite random loop.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  c proj
+//
+//  Created by Anshul  on 30/09/18.
+//  Copyright © 2018 Anshul . All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include<stdint.h>
+//#include <inttypes.h>
+int main()
+{
+    int a=1;
+    while(a<2){
+        printf("%d",rand());
+    }
+}
