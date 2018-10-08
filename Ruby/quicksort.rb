b944f26b82131608fb460fc779127cd4ea8b9e21
disha <dldisha929@gmail.com>
2018-10-09 02:06:28 +0530

    Quick sort added in Ruby

---
+++ Ruby/quicksort.rb
@@ -0,0 +1,48 @@
+def quicksort(array, from=0, to=nil)
+    if to == nil
+        # Sort the whole array, by default
+        to = array.count - 1
+    end
+
+    if from >= to
+        # Done sorting
+        return
+    end
+
+    # Take a pivot value, at the far left
+    pivot = array[from]
+
+    # Min and Max pointers
+    min = from
+    max = to
+
+    # Current free slot
+    free = min
+
+    while min < max
+        if free == min # Evaluate array[max]
+            if array[max] <= pivot # Smaller than pivot, must move
+                array[free] = array[max]
+                min += 1
+                free = max
+            else
+                max -= 1
+            end
+        elsif free == max # Evaluate array[min]
+            if array[min] >= pivot # Bigger than pivot, must move
+                array[free] = array[min]
+                max -= 1
+                free = min
+            else
+                min += 1
+            end
+        else
+            raise "Inconsistent state"
+        end
+    end
+
+    array[free] = pivot
+
+    quicksort array, from, free - 1
+    quicksort array, free + 1, to
+end
