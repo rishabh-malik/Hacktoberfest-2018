2187705f223b133fa1461a3e6119ad62f1f9b9a3
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2018-10-10 11:51:47 -0300

    helloword scala

---
+++ scala/helloWord.scala
@@ -0,0 +1,8 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
+/*
+  for more information you can go to this website https://www.scala-lang.org/documentation/your-first-lines-of-scala.html
+*/
