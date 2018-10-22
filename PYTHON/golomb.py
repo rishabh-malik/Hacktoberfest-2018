d9ed0d32ac782995524fcf5aa164a526d2388df8
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-10-23 03:30:38 +0530

    The Golomb Sequence in Python
    Python implementation of the Golomb Sequence

---
+++ PYTHON/golomb.py
@@ -0,0 +1,14 @@
+def findGolomb(n): 
+	if (n == 1): 
+		return 1
+
+	return 1 + findGolomb(n -
+	findGolomb(findGolomb(n - 1))) 
+
+def printGolomb(n):
+
+	for i in range(1, n + 1): 
+		print(findGolomb(i), end=" ")
+
+n = 15
+printGolomb(n)
