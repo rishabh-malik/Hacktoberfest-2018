5ff5c9efe5e012f2cf510551c4fd450d928f0f6d
Madson de Carvalho <madson.de.carvalho@gmail.com>
2018-10-25 17:12:40 -0300

    quicksort php

---
+++ PHP/quicksort.php
@@ -0,0 +1,38 @@
+<?php 
+
+//code from http://andrewbaxter.net/quicksort.php
+
+function quick_sort($array)
+{
+	// find array size
+	$length = count($array);
+	
+	// base case test, if array of length 0 then just return array to caller
+	if($length <= 1){
+		return $array;
+	}
+	else{
+	
+		// select an item to act as our pivot point, since list is unsorted first position is easiest
+		$pivot = $array[0];
+		
+		// declare our two arrays to act as partitions
+		$left = $right = array();
+		
+		// loop and compare each item in the array to the pivot value, place item in appropriate partition
+		for($i = 1; $i < count($array); $i++)
+		{
+			if($array[$i] < $pivot){
+				$left[] = $array[$i];
+			}
+			else{
+				$right[] = $array[$i];
+			}
+		}
+		
+		// use recursion to now sort the left and right lists
+		return array_merge(quick_sort($left), array($pivot), quick_sort($right));
+	}
+}
+
+?>
