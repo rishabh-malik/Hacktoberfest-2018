ced7b3fddb1637dad3c8ebf020c421deefb44bea
werpish2 <15963120+werpish2@users.noreply.github.com>
2018-10-26 02:03:43 -0400

    Create volCylinder.py
    cylinder volume calculator

---
+++ volCylinder.py
@@ -0,0 +1,13 @@
+# volCylinder.py calculates cylinder volume from radius, and height
+def volCylinder():
+
+    rad = float(input("What is the radius of the base of the cylider? "))
+    height = float(input("What is the height of the cylinder? "))
+    
+    vol = pi * rad * rad * height
+    
+    print("Volume is " + vol)
+    
+    return vol
+    
+volCylinder()    
