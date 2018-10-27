76372e77b50ecf88068bd484a960caaad14eb48c
Serjche <serjche@hotmail.com>
2018-10-27 15:53:26 -0500

    Added isPrime.c for checking if number is prime

---
+++ C/isPrime.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(void)
+{
+	printf("Check if number is prime");
+
+	while(get("Input number: ") != 'exit')
+	{
+
+
+	}
+}
+
+
+uint32 isPrime(unsigned int number)
+{
+
+
+	for (;;)
+	{
+
+	}
+}
