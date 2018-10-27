fb86a9c2933ab6c171ea60cdd332d83bb753582a
Serjche <serjche@hotmail.com>
2018-10-27 17:29:07 -0500

    Added placeholder values and input checks.

--- C/isPrime.c
+++ C/isPrime.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
+/* Function declaration. */
+int isPrime(long number);
 
-uint32 isPrime(unsigned int number)
-
-int main(void)
+/* Main function body. */
+int main(int argc, char *argv[])
 {
-	printf("Check if number is prime");
+	long numberToCheck = 0;
 
-	while(get("Input number: ") != 'exit')
+	if (argc < 2) /* First argument is the program name (argv[0]), which is "main". */
 	{
-
-
+		printf("Not enough arguments.\n");
+		return 1;
 	}
-}
 
-
-uint_32 isPrime(unsigned int number)
-{
-
-
-	for (;;)
+	/* TODO: Add error messages for specific invalid input. Right now there is only a generic message for all errors. */
+	/* Check if prime. */
+	if ( isPrime(atol(argv[1])) ) /* atoi returns 0 if string is not a number.*/
 	{
-
+		printf("%s is a prime number.\n", argv[1]);
 	}
+	else
+	{
+		printf("%s is not a prime number or it may be invalid (not a number or higher than LONG_MAX).\n", argv[1]);
+	}
+
 	return 0;
-}
+} /* End main */
+
+/* Function definition.*/
+int isPrime(long number)
+{
+	if (number < 2)
+	{
+		return 0; /* Placeholder value. */
+	}
+	else
+	{
+		return 1; /* Placeholder value. */
+	}
+} /* End isPrime */
