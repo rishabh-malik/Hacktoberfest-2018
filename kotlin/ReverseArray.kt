8db6a3648c033ce1db6747be39511070c56dfbf0
Ritik Banga <31412348+ritikbanga@users.noreply.github.com>
2018-10-27 23:28:19 +0530

    Update reversearray.kt

--- kotlin/ReverseArray.kt
+++ kotlin/ReverseArray.kt
@@ -1,13 +1,16 @@
 
+
+// Function to reverse arr[] from start to end
 fun reverseArray(array: ArrayList<Int>): ArrayList<Int> {
 
+   //logic to reverse an array
     for (i in 0 until array.size/2) {
         val temp = array[i]
         array[i] = array[array.size -i -1]
         array[array.size -i -1] = temp
     }
 
-    return array
+    return array   //printing the reverse array
 }
 
 
