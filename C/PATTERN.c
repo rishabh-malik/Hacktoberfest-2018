a2bdd9e7f37b1bad2012ba2b45ab33c724c39d9f
saransh2000 <saransh1000@gmail.com>
2018-10-31 18:32:03 +0530

    pattern.c
    Prints a pattern using FOR loop in C

---
+++ C/PATTERN.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main ()
+{
+   int i,j,k;
+   
+   for (i=1;i<5;i++)
+    {
+      for (j=1;j<=i;j++)
+        {
+          printf ("%d",j);
+          
+        }
+     printf ("\n"); 
+    }
+}
