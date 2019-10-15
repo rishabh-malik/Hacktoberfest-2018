7a9d0e9d67712035f4af5316b803a6c958000b93
ayush-srivastava99 <51888412+ayush-srivastava99@users.noreply.github.com>
2019-10-15 16:05:25 +0530

    Update PATTERN.c
    optimised

--- C/PATTERN.c
+++ C/PATTERN.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 void main ()
 {
-   int i,j,k;
-   
+   int i,j;
    for (i=1;i<5;i++)
     {
       for (j=1;j<=i;j++)
-        {
           printf ("%d",j);
-          
-        }
      printf ("\n"); 
     }
 }
